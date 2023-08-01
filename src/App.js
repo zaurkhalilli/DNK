3a179541377b62b0e8ac66bdac3961dfd8184ca5
zaurkhalilli <ggencetel274@gmail.com>
2023-08-01 20:53:12 +0400

    bootstrap sass added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
-function App() {
+import React from 'react'
+import Home from "./Pages/Home/Home"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./Pages/Login/Login"
+import Pricing from "./Pages/Pricing/Pricing"
+import Layout from "./Layout/Layout";
+const App = () => {
   return (
-    <div className="App">
-     
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index path='/' element={<Home/>} />
+          <Route index path='/login' element={<Login/>} />
+          <Route index path='/pricing' element={<Pricing/>} />
+        </Routes>
+    </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
