c41c4f9c7e92b027b2595923994ad04fbc425ddd
zaurkhalilli <ggencetel274@gmail.com>
2023-08-02 20:48:18 +0400

    Homeswiper added

--- src/Components/HomeSwiper/Homeswiper.jsx
+++ src/Components/HomeSwiper/Homeswiper.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import React from 'react';
 import styled from "../HomeSwiper/Homeswiper.scss"
-const Homeswiper = () => {
-  return (
-    <div className='swiper'>
-      
-    </div>
-  )
-}
+import 'swiper/less';
+import 'swiper/less/navigation';
+import 'swiper/less/pagination';
+// Import Swiper styles
+import 'swiper/css';
 
-export default Homeswiper
+export default () => {
+  return (
+  <div className="swiper">
+      <Swiper
+      spaceBetween={0}
+      slidesPerView={5}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <div className="swiperimg">
+      <SwiperSlide><img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2022/08/client-logo-5.png" alt="" /></SwiperSlide>
+      <SwiperSlide><img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2021/03/client-logo-2.png" alt="" /></SwiperSlide>
+      <SwiperSlide><img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2021/03/client-logo-3.png" alt="" /></SwiperSlide>
+      <SwiperSlide><img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2021/03/client-logo-4.png" alt="" /></SwiperSlide>
+      <SwiperSlide><img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2022/08/client-logo-5.png" alt="" /></SwiperSlide>
+      <SwiperSlide><img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2021/03/client-logo-2.png" alt="" /></SwiperSlide>
+      <SwiperSlide><img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2021/03/client-logo-3.png" alt="" /></SwiperSlide>
+      <SwiperSlide><img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2021/03/client-logo-4.png" alt="" /></SwiperSlide>
+      </div>
+    </Swiper>
+  </div>
+  );
+};
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import App from '../../App'
 import Layout from '../../Layout/Layout'
 import Hometop from '../../Components/HomeTop/Hometop'
+import Homeswiper from '../../Components/HomeSwiper/Homeswiper'
 
 const Home = () => {
   return (
     <div>
       <Layout>
         <Hometop/>
+        <Homeswiper/>
       </Layout>
     </div>
   )
