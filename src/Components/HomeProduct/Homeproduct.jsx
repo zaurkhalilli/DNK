be9e4889724d16a21d1652f45f75d1006ded470d
zaurkhalilli <ggencetel274@gmail.com>
2023-08-12 10:17:31 +0400

    grid adds

--- src/Components/HomeProduct/Homeproduct.jsx
+++ src/Components/HomeProduct/Homeproduct.jsx
@@ -6,6 +6,18 @@ const Homeproduct = () => {
      <div className="producthead">
      <h3>Featured Products</h3>
       <hr />
+      <div class="grid-container">
+      <div class="grid-item">1</div>
+      <div class="grid-item">2</div>
+      <div class="grid-item">3</div>  
+      <div class="grid-item">4</div>
+      <div class="grid-item">5</div>
+      <div class="grid-item">6</div>  
+      <div class="grid-item">7</div>
+      <div class="grid-item">8</div>
+      <div class="grid-item">9</div>  
+</div>
+
      </div>
     </div>
   )
