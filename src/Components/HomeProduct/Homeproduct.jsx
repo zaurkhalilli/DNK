d085ce66f79ee707545c981c13c5be6e0c47519e
zaurkhalilli <ggencetel274@gmail.com>
2023-08-14 22:12:47 +0400

    all items added

--- src/Components/HomeProduct/Homeproduct.jsx
+++ src/Components/HomeProduct/Homeproduct.jsx
@@ -26,11 +26,11 @@ const Homeproduct = () => {
       <div class="grid-item">
         <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2021/03/sports-shoe1-300x300.jpg" alt="" />
         <div className="grid-txt">
-        <a href="">DNK Yellow Shoes</a>
+        <a href="">DNK Blue Shoes</a>
         <h6>Men</h6>
         <div className="pricetxt">
-        <p>$150.00 </p> 
-        <span>$120.00</span>
+        <p>$240.00</p> 
+        <span>$200.00</span>
         </div>
        <div className="icons">
        <AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/>
@@ -41,11 +41,11 @@ const Homeproduct = () => {
       <div class="grid-item">
         <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-m-jeans1-300x300.jpg" alt="" />
         <div className="grid-txt">
-        <a href="">DNK Yellow Shoes</a>
+        <a href="">Dark Brown Jeans</a>
         <h6>Men</h6>
         <div className="pricetxt">
-        <p>$150.00 </p> 
-        <span>$120.00</span>
+        <span>$150.00</span>
+        <br />
         </div>
        <div className="icons">
        <AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/>
@@ -56,11 +56,11 @@ const Homeproduct = () => {
       <div class="grid-item">
         <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-w-jeans2-300x300.jpg" alt="" />
         <div className="grid-txt">
-        <a href="">DNK Yellow Shoes</a>
-        <h6>Men</h6>
+        <a href="">Blue Denim Jeans</a>
+        <h6>Women</h6>
         <div className="pricetxt">
-        <p>$150.00 </p> 
-        <span>$120.00</span>
+        <span>$150.00</span>
+        <br />
         </div>
        <div className="icons">
        <AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/>
@@ -71,11 +71,11 @@ const Homeproduct = () => {
       <div class="grid-item">
         <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-w-jeans4-300x300.jpg" alt="" />
         <div className="grid-txt">
-        <a href="">DNK Yellow Shoes</a>
-        <h6>Men</h6>
+        <a href="">Basic Gray Jeans</a>
+        <h6>Women</h6>
         <div className="pricetxt">
-        <p>$150.00 </p> 
-        <span>$120.00</span>
+        <span>$150.00</span>
+        <br />
         </div>
        <div className="icons">
        <AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/>
@@ -85,18 +85,78 @@ const Homeproduct = () => {
         </div>
       <div class="grid-item">
         <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-w-jeans1-300x300.jpg" alt="" />
+        <div className="grid-txt">
+        <a href="">Blue Denim Shorts</a>
+        <h6>Women</h6>
+        <div className="pricetxt">
+        <p>$150.00 </p> 
+        <span>$130.00</span>
+        </div>
+       <div className="icons">
+       <AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/>
+       </div>
+       
+        </div>
         </div>  
       <div class="grid-item">
         <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-accessory2-300x300.jpg" alt="" />
+        <div className="grid-txt">
+        <a href="">Anchor Bracelet</a>
+        <h6>Accessories</h6>
+        <div className="pricetxt">
+        <p>$180.00 </p> 
+        <span>$150.00</span>
+        </div>
+       <div className="icons">
+       <AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/>
+       </div>
+       
+        </div>
         </div>
       <div class="grid-item">
         <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-accessory1-300x300.jpg" alt="" />
+        <div className="grid-txt">
+        <a href="">Boho Bangle Bracelet</a>
+        <h6>Accessories</h6>
+        <div className="pricetxt">
+        <p>$170.00 </p> 
+        <span>$150.00</span>
+        </div>
+       <div className="icons">
+       <AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/>
+       </div>
+       
+        </div>
         </div>
       <div class="grid-item">
         <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-bag1-300x300.jpg" alt="" />
+        <div className="grid-txt">
+        <a href="">Light Brown Purse</a>
+        <h6>Accessories</h6>
+        <div className="pricetxt">
+        <span>$150.00</span>
+        <br />
+        </div>
+       <div className="icons">
+       <AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/>
+       </div>
+       
+        </div>
         </div>  
       <div class="grid-item">
         <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-bag3-300x300.jpg" alt="" />
+        <div className="grid-txt">
+        <a href="">Bright Red Bag</a>
+        <h6>Accessories</h6>
+        <div className="pricetxt">
+        <p>$140.00 </p> 
+        <span>$100.00</span>
+        </div>
+       <div className="icons">
+       <AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/><AiOutlineStar size={20}/>
+       </div>
+       
+        </div>
         </div>  
 </div>
 
