84dede268855abc2f93478e227dcf7f3d0afce5f
zaurkhalilli <ggencetel274@gmail.com>
2023-08-14 21:47:37 +0400

    some changes

--- src/Components/HomeProduct/Homeproduct.jsx
+++ src/Components/HomeProduct/Homeproduct.jsx
@@ -7,16 +7,37 @@ const Homeproduct = () => {
      <h3>Featured Products</h3>
       <hr />
       <div class="grid-container">
-      <div class="grid-item"><img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2021/03/sports-shoe3-300x300.jpg" alt="" /></div>
-      <div class="grid-item"><img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2021/03/sports-shoe1-300x300.jpg" alt="" /></div>
-      <div class="grid-item"><img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-m-jeans1-300x300.jpg" alt="" /></div>  
-      <div class="grid-item"><img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-w-jeans2-300x300.jpg" alt="" /></div>
-      <div class="grid-item"><img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-w-jeans4-300x300.jpg" alt="" /></div>
-      <div class="grid-item"><img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-w-jeans1-300x300.jpg" alt="" /></div>  
-      <div class="grid-item"><img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-accessory2-300x300.jpg" alt="" /></div>
-      <div class="grid-item"><img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-accessory1-300x300.jpg" alt="" /></div>
-      <div class="grid-item"><img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-bag1-300x300.jpg" alt="" /></div>  
-      <div class="grid-item"><img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-bag3-300x300.jpg" alt="" /></div>  
+      <div class="grid-item">
+        <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2021/03/sports-shoe3-300x300.jpg" alt="" />
+        
+        </div>
+      <div class="grid-item">
+        <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2021/03/sports-shoe1-300x300.jpg" alt="" />
+        </div>
+      <div class="grid-item">
+        <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-m-jeans1-300x300.jpg" alt="" />
+        </div>  
+      <div class="grid-item">
+        <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-w-jeans2-300x300.jpg" alt="" />
+        </div>
+      <div class="grid-item">
+        <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-w-jeans4-300x300.jpg" alt="" />
+        </div>
+      <div class="grid-item">
+        <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-w-jeans1-300x300.jpg" alt="" />
+        </div>  
+      <div class="grid-item">
+        <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-accessory2-300x300.jpg" alt="" />
+        </div>
+      <div class="grid-item">
+        <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-accessory1-300x300.jpg" alt="" />
+        </div>
+      <div class="grid-item">
+        <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-bag1-300x300.jpg" alt="" />
+        </div>  
+      <div class="grid-item">
+        <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2017/12/product-bag3-300x300.jpg" alt="" />
+        </div>  
 </div>
 
      </div>
