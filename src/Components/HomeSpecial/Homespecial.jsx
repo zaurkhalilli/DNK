51d79ea15e1fd397f1e74eaf1cc53527863931c1
zaurkhalilli <ggencetel274@gmail.com>
2023-08-15 22:16:07 +0400

    some changes

--- src/Components/HomeSpecial/Homespecial.jsx
+++ src/Components/HomeSpecial/Homespecial.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import styled from "../HomeSpecial/Homespecial.scss"
 const Homespecial = () => {
   return (
-    <div>
-      
+    <div className='special'>
+      <div className="specialtxt">
+        <p>Limited Time Offer</p>
+        <h3>Special Edition</h3>
+        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</span>
+        <h5>Buy This T-shirt At 20% Discount, Use Code OFF20</h5>
+        <button>SHOP NOW</button>
+      </div>
     </div>
   )
 }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,6 +5,7 @@ import Hometop from '../../Components/HomeTop/Hometop'
 import Homeswiper from '../../Components/HomeSwiper/Homeswiper'
 import Homeshop from '../../Components/HomeShop/Homeshop'
 import Homeproduct from '../../Components/HomeProduct/Homeproduct'
+import Homespecial from '../../Components/HomeSpecial/Homespecial'
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
         <Homeswiper/>
         <Homeshop/>
         <Homeproduct/>
+        <Homespecial/>
       </Layout>
     </div>
   )
