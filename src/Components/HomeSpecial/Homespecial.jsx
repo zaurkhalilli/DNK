baf826b7580a4e59d1487bb2bf6b393022b2e404
zaurkhalilli <ggencetel274@gmail.com>
2023-08-15 22:01:44 +0400

    new adds

---
+++ src/Components/HomeSpecial/Homespecial.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from "../HomeSpecial/Homespecial.scss"
+const Homespecial = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Homespecial
