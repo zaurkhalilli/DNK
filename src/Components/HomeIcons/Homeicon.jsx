12d5a19e331bda447e4429f738c15b814eafb438
zaurkhalilli <ggencetel274@gmail.com>
2023-08-15 22:46:38 +0400

    HomeIcons added

---
+++ src/Components/HomeIcons/Homeicon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from "../HomeIcons/Homeicons.scss"
+const Homeicon = () => {
+  return (
+    <div className='icons'>
+      
+    </div>
+  )
+}
+
+export default Homeicon
