8fd3291b073c6256d00797514c9fef47875d5120
zaurkhalilli <ggencetel274@gmail.com>
2023-08-15 22:29:41 +0400

    new adds

--- src/Components/HomeSpecial/Homespecial.jsx
+++ src/Components/HomeSpecial/Homespecial.jsx
@@ -5,7 +5,7 @@ const Homespecial = () => {
     <div className='special'>
       <div className="specialtxt">
         <p>Limited Time Offer</p>
-        <h3>Special Edition</h3>
+        <h2>Special Edition</h2>
         <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</span>
         <h5>Buy This T-shirt At 20% Discount, Use Code OFF20</h5>
         <button>SHOP NOW</button>
