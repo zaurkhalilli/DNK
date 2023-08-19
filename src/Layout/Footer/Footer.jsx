5e685a5cf797efe2a6f6b6bd6d4f76d5b53935b9
zaurkhalilli <ggencetel274@gmail.com>
2023-08-19 21:23:43 +0400

    footertop added

--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -3,7 +3,9 @@ import styled from "../Footer/Footer.scss"
 const Footer = () => {
   return (
     <div className='footer'>
-      
+      <div className="footertop">
+        <a href="">SALE UP TO 70% OFF FOR ALL CLOTHES & FASHION ITEMS, ON ALL BRANDS.</a>
+      </div>
     </div>
   )
 }
