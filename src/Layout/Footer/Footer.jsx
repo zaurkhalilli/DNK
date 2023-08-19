adb41f09b697c1662b95248acf9e15784af88bab
zaurkhalilli <ggencetel274@gmail.com>
2023-08-19 21:29:46 +0400

    new adds

--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -6,6 +6,18 @@ const Footer = () => {
       <div className="footertop">
         <a href="">SALE UP TO 70% OFF FOR ALL CLOTHES & FASHION ITEMS, ON ALL BRANDS.</a>
       </div>
+      <div className="footercenter">
+        <div className="container">
+          <div className="row">
+            <div className="col-xl-5">
+              
+            </div>
+            <div className="col-xl-2"></div>
+            <div className="col-xl-2"></div>
+            <div className="col-xl-3"></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
