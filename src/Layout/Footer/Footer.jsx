27bf1e074b1b45c18d35cefa6f1fe34390778442
zaurkhalilli <ggencetel274@gmail.com>
2023-08-20 23:39:59 +0400

    new adds

--- src/Layout/Footer/Footer.jsx
+++ src/Layout/Footer/Footer.jsx
@@ -14,7 +14,6 @@ const Footer = () => {
               <h3>The best look anytime, anywhere.</h3>
             </div>
             <div className="col-xl-2">
-              
               <ul>For Her
                 <li><a href="">Women Jeans</a></li>
                 <li><a href="">Tops and Shirts</a></li>
