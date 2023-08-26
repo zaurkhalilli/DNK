4576952f0169738425279d44a7a1f52e1b190139
zaurkhalilli <ggencetel274@gmail.com>
2023-08-26 22:11:32 +0400

    new adds

--- src/Components/AboutWe/Aboutwe.jsx
+++ src/Components/AboutWe/Aboutwe.jsx
@@ -6,7 +6,9 @@ const Aboutwe = () => {
       <div className="container">
         <div className="row">
             <div className="col-xl-6">
-
+                <hr />
+                <h2>Who We Are</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat auctor eu in elit.</p>
             </div>
             <div className="col-xl-6">
                 <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2018/12/slide-image-free-img.jpg" alt="" />
