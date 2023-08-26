b226478d3cfb1960600db296fbd4c877711c9d77
zaurkhalilli <ggencetel274@gmail.com>
2023-08-26 22:14:55 +0400

    new changes

--- src/Components/AboutWe/Aboutwe.jsx
+++ src/Components/AboutWe/Aboutwe.jsx
@@ -10,7 +10,6 @@ const Aboutwe = () => {
                <hr />
                 <h2>Who We Are</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat auctor eu in elit.</p>
-           
                </div>
                 </div>
             <div className="col-xl-6">
