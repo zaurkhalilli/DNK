c1eac99ce6dd98f3d69585b0e44b3d8166f1bb51
zaurkhalilli <ggencetel274@gmail.com>
2023-08-26 22:09:43 +0400

    new images added

---
+++ src/Components/AboutWe/Aboutwe.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from "../AboutWe/Aboutwe.scss"
+const Aboutwe = () => {
+  return (
+    <div className='aboutwe'>
+      <div className="container">
+        <div className="row">
+            <div className="col-xl-6">
+
+            </div>
+            <div className="col-xl-6">
+                <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2018/12/slide-image-free-img.jpg" alt="" />
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Aboutwe
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from '../../Layout/Footer/Footer'
 import Abouttop from '../../Components/AboutTop/Abouttop'
+import Aboutwe from '../../Components/AboutWe/Aboutwe'
 
 const About = () => {
   return (
     <div>
       <Abouttop/>
+      <Aboutwe/>
       <Footer/>
     </div>
   )
