8456596ffae20a76df351bda22ab7c88e26381ea
zaurkhalilli <ggencetel274@gmail.com>
2023-08-27 22:48:26 +0400

    team adds

--- src/Components/AboutTeam/Aboutteam.jsx
+++ src/Components/AboutTeam/Aboutteam.jsx
@@ -9,6 +9,30 @@ const Aboutteam = () => {
         <h2>Our Team</h2>
         <span>Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra.</span>
       </div>
+      <div className="container">
+        <div className="row">
+            <div className="col-xl-4">
+                <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2018/12/team2-free-img.png" alt="" />
+            <h3>Harvey Spector</h3>
+            <p>Founder - CEO</p>
+            </div>
+            <div className="col-xl-4">
+
+            </div>
+            <div className="col-xl-4">
+
+            </div>
+            <div className="col-xl-4">
+
+            </div>
+            <div className="col-xl-4">
+
+            </div>
+            <div className="col-xl-4">
+                
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
