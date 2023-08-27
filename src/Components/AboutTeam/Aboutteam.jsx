205b70a57c9fdbf11458181c5aa247b69ff79eca
zaurkhalilli <ggencetel274@gmail.com>
2023-08-27 22:24:56 +0400

    new adds

---
+++ src/Components/AboutTeam/Aboutteam.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from "../AboutTeam/Aboutteam.scss"
+const Aboutteam = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Aboutteam
