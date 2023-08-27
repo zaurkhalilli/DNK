5acb74bd0f52523aaf6d0eec94ba8d755bdad398
zaurkhalilli <ggencetel274@gmail.com>
2023-08-27 22:58:41 +0400

    team members added

--- src/Components/AboutTeam/Aboutteam.jsx
+++ src/Components/AboutTeam/Aboutteam.jsx
@@ -17,29 +17,29 @@ const Aboutteam = () => {
             <p>Founder - CEO</p>
             </div>
             <div className="col-xl-4">
-            <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2018/12/team2-free-img.png" alt="" />
-            <h3>Harvey Spector</h3>
-            <p>Founder - CEO</p>
+            <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2018/12/team1-free-img.png" alt="" />
+            <h3>Jessica Pearson</h3>
+            <p>COO</p>
             </div>
             <div className="col-xl-4">
-            <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2018/12/team2-free-img.png" alt="" />
-            <h3>Harvey Spector</h3>
-            <p>Founder - CEO</p>
+            <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2018/12/team3-free-img.png" alt="" />
+            <h3>Rachel Zain</h3>
+            <p>Marketing Head</p>
             </div>
             <div className="col-xl-4">
-            <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2018/12/team2-free-img.png" alt="" />
-            <h3>Harvey Spector</h3>
-            <p>Founder - CEO</p>
+            <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2018/12/team4free-img.png" alt="" />
+            <h3>Luise Litt</h3>
+            <p>Lead Developer</p>
             </div>
             <div className="col-xl-4">
-            <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2018/12/team2-free-img.png" alt="" />
-            <h3>Harvey Spector</h3>
-            <p>Founder - CEO</p>
+            <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2018/12/team5-free-img.png" alt="" />
+            <h3>Katrina Bennett</h3>
+            <p>Intern Designer</p>
             </div>
             <div className="col-xl-4">
-            <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2018/12/team2-free-img.png" alt="" />
-            <h3>Harvey Spector</h3>
-            <p>Founder - CEO</p>
+            <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2018/12/team6-free-img.png" alt="" />
+            <h3>Mike Ross</h3>
+            <p>Intern Designer</p>
             </div>
         </div>
       </div>
