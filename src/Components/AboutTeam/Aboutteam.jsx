d1dcf3b54488b3783ee11d90680a9da8024abdad
zaurkhalilli <ggencetel274@gmail.com>
2023-08-27 22:32:11 +0400

    some changes

--- src/Components/AboutTeam/Aboutteam.jsx
+++ src/Components/AboutTeam/Aboutteam.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import styled from "../AboutTeam/Aboutteam.scss"
 const Aboutteam = () => {
   return (
-    <div>
-      
+    <div className='team'>
+      <div className="teamtop">
+        <hr />
+        <p>A Few Words About</p>
+        <h2>Our Team</h2>
+        <span>Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra.</span>
+      </div>
     </div>
   )
 }
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Footer from '../../Layout/Footer/Footer'
 import Abouttop from '../../Components/AboutTop/Abouttop'
 import Aboutwe from '../../Components/AboutWe/Aboutwe'
+import Aboutteam from '../../Components/AboutTeam/Aboutteam'
 
 const About = () => {
   return (
     <div>
       <Abouttop/>
       <Aboutwe/>
+      <Aboutteam/>
       <Footer/>
     </div>
   )
