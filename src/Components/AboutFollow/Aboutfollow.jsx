4b694f1fee97395fa0e6897cb1907eb216e94e54
zaurkhalilli <ggencetel274@gmail.com>
2023-08-28 22:33:52 +0400

    new adds

--- src/Components/AboutFollow/Aboutfollow.jsx
+++ src/Components/AboutFollow/Aboutfollow.jsx
@@ -3,7 +3,24 @@ import styled from "../AboutFollow/Aboutfollow.scss"
 const Aboutfollow = () => {
   return (
     <div className='follow'>
-      
+      <div className="followtxt">
+        <hr />
+        <h2>Follow Us</h2>
+        <div className="followicons">
+        <div className="facebook">
+
+        </div>
+        <div className="twitter">
+
+        </div>
+        <div className="instagram">
+
+        </div>
+        <div className="google">
+            
+        </div>  
+        </div>
+      </div>
     </div>
   )
 }
