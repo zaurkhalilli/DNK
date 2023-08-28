414065462187cc64d30787cd5269e3024d7cf023
zaurkhalilli <ggencetel274@gmail.com>
2023-08-28 22:30:31 +0400

    new adds

---
+++ src/Components/AboutFollow/Aboutfollow.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from "../AboutFollow/Aboutfollow.scss"
+const Aboutfollow = () => {
+  return (
+    <div className='follow'>
+      
+    </div>
+  )
+}
+
+export default Aboutfollow
