e0490cc5b5582c53cc332fc568ac08aac327457e
zaurkhalilli <ggencetel274@gmail.com>
2023-08-29 21:11:04 +0400

    login page image added

--- src/Components/LoginPage/Login.jsx
+++ src/Components/LoginPage/Login.jsx
@@ -4,7 +4,9 @@ const Login = () => {
   return (
     <div className='login'>
       <img src="https://websitedemos.net/brandstore-02/wp-admin/images/wordpress-logo.svg?ver=20131107" alt="" />
-        
+        <div className="container">
+            
+        </div>
     </div>
   )
 }
