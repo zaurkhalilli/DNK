7880f24e308d03bc040656c4ac742f8add81cf47
zaurkhalilli <ggencetel274@gmail.com>
2023-08-29 21:10:16 +0400

    image added

--- src/Components/LoginPage/Login.jsx
+++ src/Components/LoginPage/Login.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import styled from "../LoginPage/Login.scss"
 const Login = () => {
   return (
-    <div>
-      
+    <div className='login'>
+      <img src="https://websitedemos.net/brandstore-02/wp-admin/images/wordpress-logo.svg?ver=20131107" alt="" />
     </div>
   )
 }
