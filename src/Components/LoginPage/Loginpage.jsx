50e9aca8d523cda5dbab6aff542ae4acf1e849b0
zaurkhalilli <ggencetel274@gmail.com>
2023-08-30 15:13:06 +0400

    new adds

--- src/Components/LoginPage/Loginpage.jsx
+++ src/Components/LoginPage/Loginpage.jsx
@@ -6,10 +6,16 @@ const Login = () => {
       <img src="https://websitedemos.net/brandstore-02/wp-admin/images/wordpress-logo.svg?ver=20131107" alt="" />
         <div className="container">
             <label htmlFor="">Username or Email Address</label>
+            <br />
             <input type="text" />
+            <br />
             <label htmlFor="">Password</label>
+            <br />
             <input type="password" />
+            <br />
+            <div className="loginbtn">
             <button>Login</button>
+            </div>
         </div>
     </div>
   )
