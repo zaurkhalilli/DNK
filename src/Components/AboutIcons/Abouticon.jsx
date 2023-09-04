e575401cc765122d7bbfd6544d09fc916b3637f2
zaurkhalilli <ggencetel274@gmail.com>
2023-09-04 22:44:06 +0400

    about icon page new adds

--- src/Components/AboutIcons/Abouticon.jsx
+++ src/Components/AboutIcons/Abouticon.jsx
@@ -6,6 +6,7 @@ const Abouticon = () => {
        <div className="container">
         <div className="row">
             <div className="col-xl-3">
+              
                 <img src="https://websitedemos.net/brandstore-02/wp-content/uploads/sites/150/2018/12/globe-free-img.png" alt="" />
                 <p>Worldwide Shipping</p>
                 <span>It elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</span>
